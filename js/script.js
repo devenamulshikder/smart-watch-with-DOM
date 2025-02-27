dd379d8fa353aa50d7ddb4fe21a7d349224b3ae7
devenamulshikder <devenamulshikder@gmail.com>
2025-02-28 00:58:27 +0600

    Add favicon and enhance button interaction in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert('alert')
+const ringButtons = document.querySelectorAll(".ring-button");
+for (let i = 0; i < ringButtons.length; i++) {
+  const ringBtn = ringButtons[i];
+  ringBtn.addEventListener("click", function (e) {
+    for (let j = 0; j < ringButtons.length; j++) {
+      ringButtons[j].classList.remove("border-purple-600");
+      ringButtons[j].classList.add("border-gray-100");
+    }
+    e.target.classList.add("border-purple-600");
+  });
+}
